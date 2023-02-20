1ab7f2f284b778d425b5dd40e9ac82a18e921b8c
fily1212 <fily1212@gmail.com>
2023-02-20 11:18:12 +0100

    input

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,11 @@
 // 
 public class HelloWorld{
-
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+        int a; // Dichiarazione di una variabile
+        a = 10; // assegnamento
+        int b = 5; // Dichiarazione e assegnamento
+        System.out.println(a); 
+        
+    }
 }
