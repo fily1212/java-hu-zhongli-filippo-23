91a0716ec22c145e7b1e31acf0a9262d2f578a9b
fily1212 <fily1212@gmail.com>
2023-02-24 12:06:22 +0100

    lezione 3

--- Es/src/lezione3/DataCorretta.java
+++ Es/src/lezione3/DataCorretta.java
@@ -1,2 +1,27 @@
-package lezione3;public class DataCorretta {
+package lezione3;
+
+import java.util.Scanner;
+
+public class DataCorretta {
+    public static void main(String[] args) {
+        Scanner tastiera = new Scanner(System.in);
+        System.out.println("Inserisci un giorno: ");
+        int giorno = tastiera.nextInt();
+        System.out.println("Inserisci un mese: ");
+        int mese = tastiera.nextInt();
+        int soglia = 31;
+        if(mese == 2)
+            soglia = 28;
+        else if(mese == 1 || mese == 3 || mese == 5 || mese == 7 ||
+                mese == 8 || mese == 10 || mese == 12)
+            soglia = 31;
+        else if(mese == 4 || mese == 6 || mese == 9 || mese == 11)
+            soglia = 30;
+        boolean corretto = 1 <= mese && mese <= 12  &&
+                1 <= giorno && giorno <= soglia;
+
+
+        if(corretto) System.out.println("OK");
+        else System.out.println("NO");
+    }
 }
--- Es/src/lezione3/EsIf.java
+++ Es/src/lezione3/EsIf.java
@@ -1,2 +1,20 @@
-package lezione3;public class EsIf {
+package lezione3;
+
+import java.util.Scanner;
+
+public class EsIf {
+    public static void main(String[] args) {
+        /* Inserisci un nome, se inizia per A
+        scrivi ciao altrimenti buongiorno */
+        Scanner tastiera = new Scanner(System.in);
+        System.out.println("Inserisci il tuo nome");
+        String nome = tastiera.nextLine();
+        if (nome.substring(0,1).equalsIgnoreCase("A"))
+            System.out.println("Ciao");
+        else
+            System.out.println("Buongiorno");
+
+        /* inserire giorno e mese e dire se può essere una data*/
+
+    }
 }
