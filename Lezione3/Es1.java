d986755944a0cc7abbbd9458c0206c2f582e7af6
fily1212 <fily1212@gmail.com>
2023-02-24 12:06:36 +0100

    lezione 3

---
+++ Lezione3/Es1.java
@@ -0,0 +1,11 @@
+public class Es1 {
+    public static void main(String[] args) {
+        double b = 0.1;
+        int a = (int)b;
+        char c = (char)97;
+        System.out.println(a);
+        System.out.println(c);
+        
+    }
+    
+}
---
+++ Lezione3/Selezione.java
@@ -0,0 +1,20 @@
+public class Selezione {
+    public static void main(String[] args) {
+        /* if
+        if(CONDIZIONE){
+
+        }
+        // if-else
+        if(CONDIZIONE){
+
+        }else{
+
+        }*/
+
+        int a = 0;
+        if(a == 0){
+            System.out.println("OK");
+        }
+
+    }
+}
