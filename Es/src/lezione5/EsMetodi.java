afc6861b204c982768767d6a73f2f693b10b22a8
fily1212 <fily1212@gmail.com>
2023-03-01 08:39:41 +0100

    lezione 5

---
+++ Es/src/lezione5/EsMetodi.java
@@ -0,0 +1,34 @@
+package lezione5;
+
+public class EsMetodi {
+    /** metodo dado() che restituisce un numero da 1 a 6. Usare la classe Random
+     * @return      int da 1 a 6
+     */
+
+
+    /**
+     * metodo tuttiPari(int n) che chiede n numeri e ritorna true se sono tutti pari
+     * non usare array (non memorizzare i numeri)
+     * @param   n   quanti numeri chiedere
+     * @return      true se tutti i numeri chiesti sono pari, altrimenti false
+     */
+
+    /**
+    * metodo soloVocali(String s) che restituisce le vocali della stringa
+     * esempio: "Ciao come stai?" -> "iaooeai"
+     * @param   s   una stringa
+     * @return      le vocali della stringa
+     * */
+
+    /*
+    * Scrivere un programma ContinuaFino che chiede all'utente di inserire una sequenza di
+interi. Il programma continua a chiedere all'utente di inserire valori
+ tanto che i valori inseriti soddisfano
+almeno una delle seguenti condizioni:
+- è negativo e divisibile per 2
+- è positivo e divisibile per 3
+Al termine il programma stampa la somma di tutti i valori inseriti escluso quello ha violato le condizioni.
+Risolvere questo esercizio senza usare array.
+* */
+
+}
