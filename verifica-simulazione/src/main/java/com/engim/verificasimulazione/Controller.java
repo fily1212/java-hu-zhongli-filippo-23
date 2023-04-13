bbdbed2ce431ee2d387df188e65af9f29cff8718
fily1212 <fily1212@gmail.com>
2023-04-14 00:10:29 +0200

    simulazione verifica

---
+++ verifica-simulazione/src/main/java/com/engim/verificasimulazione/Controller.java
@@ -0,0 +1,22 @@
+package com.engim.verificasimulazione;
+
+public class Controller {
+    /*
+    * ES 1: get ("/add?nome=n") che aggiunge un nome n per il sorteggio (utilizzare la classe Sorteggio) 20 p
+    * ES 2: get ("/sorteggia?num=n") che restituisce n numeri casuali (utilizzare la classe Sorteggio) 20 p
+    * ES 3: get ("/listaNomi?sottostringa=s") che restituisce la lista dei nomi che contengono la sottostringa s 20 p
+    * ES 4: creare la classe SorteggioRuolo in cui, dopo aver aggiunto i nomi ("/add?nome=n"),
+    * sorteggia per ogni nome un ruolo di Lupus (get: "/sorteggia")
+    * se i giocatori sono meno di 7 non sorteggia. con 8 sorteggia 5 VILLICI, 2 LUPI, 1 VEGGENTE
+    * 9: precedenti + 1 MEDIUM
+    * 10: + 1 INDEMONIATO
+    * 11: + 1 PROTETTORE
+    * 12: + 1 VILLICO
+    * 13: + 1 LUPO
+    * 14: + 1 VILLICO
+    * 15: + 1 INDEMONIATO
+    * Non consente l'aggiunta di più di 15 giocatori.
+    * creare get ("/getRuoli") che restituisce tutti i ruoli e get ("getRuolo?nome=n")
+    * che restituisce il ruolo del singolo giocatore  25 p
+    * */
+}
